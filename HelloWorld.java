b03d3d95a2d0ee5c23f226a2015f40140a79fae1
julissaf <47190736+julissaf@users.noreply.github.com>
2019-06-11 12:43:23 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+//Julissa Franco
+//05-07-2019
+
+/*Module 5 Problem 1 
+  Prints Hello World */
+
+
+class HelloWorld
+{
+	public static void main(String []args)
+	{
+		System.out.println("Hello World");
+	}
+}
---
+++ Problem2.java
@@ -0,0 +1,13 @@
+
+
+
+class problem2
+{
+    public static void main(String []args)
+	{
+		int sampleNumber = 5;
+		
+		System.out.println(sampleNumber);
+	}
+
+}
